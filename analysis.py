e98dbb297e8203cb30a5f4f907a972068f3a4749
again13 <againhoffman@yahoo.com>
2022-12-03 19:37:43 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -7,3 +7,4 @@ Temporary commit content for 2022-10-19T19:37:43
 Temporary commit content for 2022-10-31T19:37:43
 Temporary commit content for 2022-11-11T19:37:43
 Temporary commit content for 2022-11-19T19:37:43
+Temporary commit content for 2022-12-03T19:37:43
