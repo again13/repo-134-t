0ef77a5a4da262e84db6cb8f9398076d632ce1fa
again13 <againhoffman@yahoo.com>
2023-04-05 19:37:43 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -20,3 +20,4 @@ Temporary commit content for 2023-02-13T19:37:43
 Temporary commit content for 2023-02-15T19:37:43
 Temporary commit content for 2023-03-03T19:37:43
 Temporary commit content for 2023-03-07T19:37:43
+Temporary commit content for 2023-04-05T19:37:43
