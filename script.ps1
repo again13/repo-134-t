30a597a3d42f8cbfd11e6e24022f280934c095f5
again13 <againhoffman@yahoo.com>
2023-08-20 19:37:43 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -33,3 +33,4 @@ Temporary commit content for 2023-07-22T19:37:43
 Temporary commit content for 2023-07-23T19:37:43
 Temporary commit content for 2023-07-24T19:37:43
 Temporary commit content for 2023-08-02T19:37:43
+Temporary commit content for 2023-08-20T19:37:43
